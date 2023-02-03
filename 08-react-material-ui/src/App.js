2f22d6bd107003689df805bf79ba6a1970855965
SamuelHurtado20 <samuelhurtado20@hotmail.com>
2023-02-02 19:53:42 -0500

    react and materialUi

---
+++ 08-react-material-ui/src/App.js
@@ -0,0 +1,11 @@
+import PersistentDrawerLeft from "./components/PersistentDrawerLeft";
+
+function App() {
+  return (
+    <div className="App">
+      <PersistentDrawerLeft />
+    </div>
+  );
+}
+
+export default App;

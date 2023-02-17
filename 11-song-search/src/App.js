a66b5d225761746ec0f1c174b92887306b1801b2
SamuelHurtado20 <samuelhurtado20@hotmail.com>
2023-02-16 21:59:11 -0500

    several improvements

---
+++ 11-song-search/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SongSearch from './components/SongSearch';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Example react: Song Search</h1>
+    <hr />
+    <SongSearch />
+    </div>
+  );
+}
+
+export default App;
